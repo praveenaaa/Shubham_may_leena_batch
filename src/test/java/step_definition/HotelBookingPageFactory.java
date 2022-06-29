0a619e7d27f53a372dbb70c99e207d74cf49ae42
praveenaaa <48066696+praveenaaa@users.noreply.github.com>
2022-06-29 12:30:40 +0530

    Update HotelBookingPageFactory.java
    @MayurY-Automation  please make some changes in the code 
    @hemanthke96 try to complete some scenario.
    @sanketg99 plz make some changes

--- src/test/java/step_definition/HotelBookingPageFactory.java
+++ src/test/java/step_definition/HotelBookingPageFactory.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
-
+//mayur please write all the implementation logic 
 public class HotelBookingPageFactory {
 WebDriver driver;
 	
